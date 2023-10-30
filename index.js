435ae8c5bd9699f97df3a616ea504300066354a2
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-10-29 17:13:00 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let greeting 
+  if(feet <= 400) {
+    greeting =  'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    greeting = 'That will be twenty bucks.'
+  } else if (feet > 2000 && feet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+  } else {
+    greeting = 'No can do.'
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ let greeting;
+  switch (tip) {
+    case 'generous': return 'Thank you so much.';
+    case 'not as generous': return 'Thank you.';
+    default: return 'Bye.';
+  }
 }
